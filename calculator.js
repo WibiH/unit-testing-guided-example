23a97e3d5cb3a1a4095d7b7096f85d9e20dea9d3
Wiebke Hagenauer <wiebosaurus@hotmail.com>
2022-08-06 17:19:42 +0200

    pass some test

--- calculator.js
+++ calculator.js
@@ -1,13 +1,36 @@
 function sum(a, b) {
-  return;
+if (!a){
+  return 0 + b;
+} else if (!b){
+  return a + 0;
+} 
+// I didn´t manage this one :(
+else if (!a && !b){
+  return 0 + 0;
+}
+else {
+    return a + b;
+}
 }
 
 function subtract(a, b) {
-  return;
+  if (!a){
+    return 0-b;
+  } else if (!b) {
+    return a - 0;
+  } else if (!a && !b){
+    return 0 - 0;
+  } else {
+    return a - b;
+}
 }
 
 function divide(a, b) {
-  return;
+  if (b === 0){
+    throw new Error('An explanatory error message');
+} else {
+return a / b;
+  }
 }
 
 function multiply(a, b) {
